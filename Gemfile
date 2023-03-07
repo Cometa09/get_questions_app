64149bc3fb3a63d2abcffdcca21e410f3377fea8
Cometa09 <sokolovalex09@gmail.com>
2023-03-07 15:25:58 +0000

    Create CRUD funtions

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
 
+
+gem 'turbo-rails', '~> 1.0'
 # Use Sass to process CSS
 # gem "sassc-rails"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get '/questions', to: 'questions#index'
+
+  resources :questions
+
+  #get '/questions', to: 'questions#index'
+  #get '/questions/new', to: 'questions#new'
+  #get '/questions/:id/edit', to: 'questions#edit'
+  #post '/questions', to: 'questions#create'
   
   root 'pages#index'
 end
