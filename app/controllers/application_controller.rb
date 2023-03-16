137e1e4c0c42808ab67130b2ab67e4aa756445a2
Cometa09 <sokolovalex09@gmail.com>
2023-03-16 10:56:45 +0000

    add call 404, if page not found v2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
-  rescue_from ActiveRecord::RecordNotFound, with: :notfound
-  #для ошибки "ActiveRecord::RecordNotFound" вызвать следующий метод
-  
-  private
-
-  def notfound(exception)
-    logger.warn exception
-    render file: 'public/404.html', status: :not_found, layout: false
-  end
+  include ErrorHandling
 end
