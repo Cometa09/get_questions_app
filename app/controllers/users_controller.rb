2676a3274ab68b65cb54075518636fa2cde839e3
Cometa09 <sokolovalex09@gmail.com>
2023-05-29 13:24:11 +0000

    add Log_in func, upgrade sign_in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,15 +6,17 @@ class UsersController < ApplicationController
     def create
         @user = User.new user_params
         if @user.save
-            session[:user_id] = @user.id
-            flash[:success] = "Welcome to the app, #{@user.name}!"
+            sign_in @user
+            flash[:success] = "Welcome to the app, #{current_user.name}!"
             redirect_to root_path
         else
-        render :new
+            render :new
         end
     end
 
+    private
+
     def user_params
-        params.require(:user).permit(:email, :name, :password, :password_confirmation)
+        params.require(:user).permit(:email, :name, :password, :password_confirmation, :old_password)
     end
 end
