286055e4facf65d571ade8c0054baafec32131f9
Cometa09 <sokolovalex09@gmail.com>
2023-06-08 14:49:29 +0000

    add remember mail field, when get error, + fix flash bug when render new

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  attr_accessor :old_password
+  attr_accessor :old_password, :remember_token
 
   has_secure_password validations: false
 
@@ -13,7 +13,28 @@ class User < ApplicationRecord
   # rubocop:enable Rails/UniqueValidationWithoutIndex
   validate :password_complexity
 
+  def remember_me
+    self.remember_token = SecureRandom.urlsafe_base64
+    update_column :remember_token_digest, digest(remember_token)
+  end
+
+  def forget_me
+    update_column :remember_token_digest, nil
+  end
+
+  def remember_token_authenticated?(remember_token)
+    return fals unless remember_token_digest.present?
+    
+    BCrypt::Password.new(remember_token_digest).is_password?(remember_token)
+  end
+
   private
+
+  def digest(string)
+    cost = ActiveModel::SecurePassword.
+      min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 
   def correct_old_password
     return if BCrypt::Password.new(password_digest_was).is_password?(old_password)
